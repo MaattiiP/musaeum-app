5d56ab99d270dbd4100b1593ed37aac0763bab3b
Seba <38462439+Courier-Seba@users.noreply.github.com>
2019-07-21 14:59:51 -0300

    Initial navbar (#2)
    * Add out meta-data
    
    * Install bulma
    
    * Deletes react strap stuff
    
    * Change app css for sass
    
    * Add bulma style (global)
    
    * Add header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import logo from './musaeum-logo.svg';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="navbar">
+        <div class="navbar-brand">
+          <img src={logo} alt="Logo" className="navbar-item image is-rounded is-64x64" />
+          <h1 className="navbar-item">Musaeum</h1>
+        </div>
+      </div>
+    )
+  }
+}
