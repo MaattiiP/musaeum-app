1962382534651e7fda7da8f24faf06ebabb298fa
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-07-24 19:36:22 -0300

    Install content app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.sass';
 
 import Header from './components/header/Header';
+import Content from './components/content/Content.jsx';
 
 function App() {
   return (
     <div className="App container">
       <Header />
+      <Content />
 
     </div>
   );
