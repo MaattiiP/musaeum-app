59b812a99ac9d1076450fbea80e36e112e8fe2bc
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-07-26 23:15:29 -0300

    Refactor content

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,8 +1,13 @@
-import React, { Component } from 'react'
-import api from '../../api/api'
-import MuseumBox from './museum-box/MuseumBox'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class Content extends Component {
+import api from '../../api/api';
+import { saveMuseums } from "./actions";
+
+import MuseumBox from './museum-box/MuseumBox';
+
+
+class Content extends Component {
   constructor(props) {
     super(props)
 
@@ -14,23 +19,20 @@ export default class Content extends Component {
   componentWillMount() {
     api.getListMuseum().then(
       response => {
-        this.setState(state => ({
-          response: response.data.results
-        }))
+        this.props.saveMuseums(response.data.results)
       }
     )
   }
 
 
   render() {
-    let museumList = this.state.response
+    let museumList = this.props.museums.museumsList
     const listOfMuseums = museumList.map((museum) =>
       <MuseumBox
         key={museum.id}
         name={museum.short_name}
         country={museum.country}
         city={museum.city}
-
       />
     );
     return (
@@ -39,4 +41,13 @@ export default class Content extends Component {
       </div>
     )
   }
-}
+};
+
+const mapStateToProps = state => ({
+  museums: state.museums
+});
+const mapDispatchToProps = dispatch => ({
+  saveMuseums: (payload) => dispatch(saveMuseums(payload))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Content)
---
+++ src/components/content/actions.js
@@ -0,0 +1,9 @@
+import { GET_MUSEUMS  }from './actionsTypes';
+
+
+export function saveMuseums(payload) {
+  return {
+    type: GET_MUSEUMS,
+    museums: payload
+  }
+}
