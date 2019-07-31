f9aa9489d93d843e18066b1dd10b0b79f41ab99c
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-07-31 13:01:41 -0300

    Add header reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 
 import museumReducer from '../components/content/museum-display/reducers';
 import artifactReducer from '../components/content/artifact-display/reducers';
+import headerReducer from '../components/header/reducers';
 
 
 const rootReducer = combineReducers({
   museums: museumReducer,
-  artifacts : artifactReducer
+  artifacts : artifactReducer,
+  header: headerReducer
 })
 
 export default rootReducer;
