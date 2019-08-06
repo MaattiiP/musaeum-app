a9c6c205be1a3993e5b478cc835f15b4f70e1f03
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-08-06 16:52:36 -0300

    Add artifact modal

--- src/components/content/artifact-display/artifact-box/ArtifactBox.jsx
+++ src/components/content/artifact-display/artifact-box/ArtifactBox.jsx
@@ -1,16 +1,33 @@
 import React, { Component } from "react";
+import ArtifactModel from "./ArtifactModel";
 
 export default class ArtifactBox extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      modalStatus: false
+    }
+    this.showStatus = this.showStatus.bind(this);
+  }
+  showStatus() {
+    this.setState({
+      modalStatus: !this.state.modalStatus
+    })
+  }
   render() {
     return (
       <div className="column is-narrow">
-        <div className="box">
+        <ArtifactModel 
+          modalStatus={this.state.modalStatus}
+          closeCallback={this.showStatus}
+          artifactData={this.props}
+        />
+        <div className="box" onClick={this.showStatus}>
           <div className="content has-text-centered">
             <figure className="image is-128x128">
               <img src={this.props.picture} alt="X"/>
             </figure>
             <h1 className="title is-4">{this.props.name}</h1>
-            <p className="is-5">{this.props.description}</p>
           </div>
         </div>
       </div>
