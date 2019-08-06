f233992f4e3c986be2aca03d67ef886b6cda3c30
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-08-06 13:59:10 -0300

    Fix footer props

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
         <div className="section">
           <SignUp />
           <Content />
+        </div>
       </div>
       <div className="hero-foot">
         <Footer />
